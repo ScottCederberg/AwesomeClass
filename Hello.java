cfa152bee5292163f73ec7457795c867fe992a78
Scott Cederberg <cederber@gmail.com>
2012-02-16 00:22:00 -0800

    Making Hello.java fancier.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
 
   public static void main(String[] args) {
-    System.out.println("Hello, git users!  You're looking awesome today!");
+    if (args.length > 0 && "-f".equals(args[0])) {
+      System.out.println("~~~ You're a fancy fellow.  Here's a fancy greeting. ~~~");
+      System.out.println("___ Ooooh la la ___");
+    } else {
+      System.out.println("Hello, git users!  You're looking awesome today!");
+    }
   }
-
 }
