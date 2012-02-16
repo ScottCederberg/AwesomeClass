2e7ac72091151410f5c2f676602c9b173b15c319
Scott Cederberg <Scott.Cederberg@gmail.com>
2012-02-16 01:04:21 -0800

    Revert "Working on adding '-n' option for user's name."
    This reverts commit 29a63c540612b6a879d9c17d4303e003859e43c5.

--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
     String greeting;
-    String name = "";
-    if (args.length > 21 && "-n".equals(args[1])) {
-      name = args[2];
-    }
     if (args.length > 0 && "-f".equals(args[0])) {
       greeting = new FancyGreetingProvider().getGreeting();
     } else {
